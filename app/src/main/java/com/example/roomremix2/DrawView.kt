44f821bd284ed01594540573c6610d83780175d6
ProvidenceV <grisbyar@mail.uc.edu>
2023-02-26 11:59:17 -0500

    Fixed Doc

---
+++ app/src/main/java/com/example/roomremix2/DrawView.kt
@@ -0,0 +1,82 @@
+package com.example.roomremix2
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import com.example.roomremix2.MainActivity.Companion.drawTool
+import com.example.roomremix2.MainActivity.Companion.path
+
+class DrawView : View {
+
+    var params : ViewGroup.LayoutParams? = null
+
+    companion object{
+        var pathList = ArrayList<Path>() // stores all paths drawn on screen
+        var colorList = ArrayList<Int>()
+        var currentColor = Color.BLACK
+    }
+    //import constructor view
+    constructor(context: Context) : this(context, null){
+        init()
+    }
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0){
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+    private fun init(){
+        /* initialize function
+        makes texture smooth for drawing */
+        drawTool.isAntiAlias = true
+        drawTool.color = currentColor
+        drawTool.style = Paint.Style.STROKE
+        drawTool.strokeJoin = Paint.Join.ROUND
+        drawTool.strokeWidth = 8f
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        /*register draw movement on screen*/
+
+
+        var xCord = event.x
+        var yCord = event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+                //detect when user is pressing down on screen
+                path.moveTo(xCord, yCord)
+                return true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                //detect when user is moving on screen
+                path.lineTo(xCord,yCord)
+                pathList.add(path)
+                colorList.add(currentColor)
+            }
+            else -> return false
+        }
+        postInvalidate()
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+
+        for(i in pathList.indices){
+            drawTool.setColor(colorList[i])
+            canvas.drawPath(pathList[i],drawTool)
+            invalidate()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/roomremix2/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.roomremix2
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
+import com.example.roomremix2.DrawView.Companion.colorList
+import com.example.roomremix2.DrawView.Companion.currentColor
+import com.example.roomremix2.DrawView.Companion.pathList
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        //import tools to draw
+        //equivalent of public static
+
+        var path = Path()
+        var drawTool = Paint()
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val drawButton = findViewById<ImageButton>(R.id.draw);
+        val eraserButton = findViewById<ImageButton>(R.id.eraser);
+        val deleteButton = findViewById<ImageButton>(R.id.delete)
+
+        drawButton.setOnClickListener{
+            //Toast.makeText(this,"Clicked draw", Toast.LENGTH_SHORT).show()
+            drawTool.color = Color.BLACK
+            currentColorPicker(drawTool.color)
+        }
+        eraserButton.setOnClickListener {
+            //Toast.makeText(this,"Clicked erase", Toast.LENGTH_SHORT).show()
+            drawTool.color = Color.WHITE
+            currentColorPicker(drawTool.color)
+        }
+        deleteButton.setOnClickListener {
+            //Toast.makeText(this,"Clicked delete", Toast.LENGTH_SHORT).show()
+            pathList.clear()
+            colorList.clear()
+            path.reset()
+        }
+
+    }
+
+    private fun currentColorPicker(color: Int){
+        currentColor = color
+        path = Path()
+    }
+}
