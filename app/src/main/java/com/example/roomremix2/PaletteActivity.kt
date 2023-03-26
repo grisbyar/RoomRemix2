452438bff7196f1906c11224a6dad3337554b113
GrassMaintenance <benji@fuse.net>
2023-03-26 15:37:00 -0400

    Added bottom bar to all other activities except for main activity.

---
+++ app/src/main/java/com/example/roomremix2/PaletteActivity.kt
@@ -0,0 +1,39 @@
+package com.example.roomremix2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class PaletteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_palette)
+
+        val decoratorButton = findViewById<Button>(R.id.decorator)
+        val shoppingButton = findViewById<Button>(R.id.shopping)
+        val ideasButton = findViewById<Button>(R.id.ideas)
+        val paletteButton = findViewById<Button>(R.id.palette)
+        val accountButton = findViewById<Button>(R.id.account)
+
+        decoratorButton.setOnClickListener(){
+            Toast.makeText(this, "Clicked decorator", Toast.LENGTH_SHORT)
+        }
+
+        shoppingButton.setOnClickListener(){
+            Toast.makeText(this, "Clicked shopping", Toast.LENGTH_SHORT)
+        }
+
+        ideasButton.setOnClickListener(){
+            Toast.makeText(this, "Clicked ideas", Toast.LENGTH_SHORT)
+        }
+
+        paletteButton.setOnClickListener(){
+            Toast.makeText(this, "Clicked palette", Toast.LENGTH_SHORT)
+        }
+
+        accountButton.setOnClickListener(){
+            Toast.makeText(this, "Clicked account", Toast.LENGTH_SHORT)
+        }
+    }
+}
