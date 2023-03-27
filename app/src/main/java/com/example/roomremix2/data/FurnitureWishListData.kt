729cca21608c97ebf5aa18c51139721b69250c32
ProvidenceV <grisbyar@mail.uc.edu>
2023-03-26 20:52:53 -0400

    Added Wishlist data class

---
+++ app/src/main/java/com/example/roomremix2/data/FurnitureWishListData.kt
@@ -0,0 +1,17 @@
+package com.example.roomremix2.data
+
+class FurnitureWishListData {
+    //this will link google shopping to here and store wish list items
+    data class Product(
+        val title: String,
+        val link: String,
+        val imageLink: String,
+        val price: String
+    )
+
+    data class WishlistItem(
+        val title: String,
+        val link: String,
+        val imageLink: String
+    )
+}
