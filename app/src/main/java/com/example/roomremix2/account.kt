ceaaf1dc4872fcaf10f3aa154c140712bbc527ee
kenkelbt <bkenks@gmail.com>
2023-03-26 18:14:52 -0600

    Added bottom navigation bar with icons.

---
+++ app/src/main/java/com/example/roomremix2/account.kt
@@ -0,0 +1,12 @@
+package com.example.roomremix2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.roomremix2.R
+
+class account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account)
+    }
+}
