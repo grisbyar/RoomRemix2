943486163bfdf8fc173e799c84a9083c25d74838
kenkelbt <kenkelbt@mail.uc.edu>
2023-04-09 20:56:58 -0400

    Fixed hardcoded ui

--- app/src/main/java/com/example/roomremix2/account.kt
+++ app/src/main/java/com/example/roomremix2/account.kt
@@ -2,6 +2,7 @@ package com.example.roomremix2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.roomremix2.R
 import com.example.roomremix2.databinding.ActivityMainBinding
 
@@ -14,7 +15,17 @@ class account : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_account)
 
-        binding.
+        val txtName: TextView = findViewById(R.id.txtviewName)
+        val txtAcc: TextView = findViewById(R.id.txtviewAcc)
+        val txtEmail: TextView = findViewById(R.id.txtviewEmail)
+        val txtEFill: TextView = findViewById(R.id.txtviewEmailFill)
+        val txtNFill: TextView = findViewById(R.id.txtviewNameFill)
+
+        txtName.text = "Name"
+        txtAcc.text = "Account"
+        txtEmail.text = "Email"
+        txtEFill.text = "EmailFill"
+        txtNFill.text = "NameFill"
     }
 
 
