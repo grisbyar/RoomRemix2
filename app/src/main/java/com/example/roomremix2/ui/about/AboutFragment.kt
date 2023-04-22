ea192c3a3cad2939f047a5072bcb28aed56a24ea
ProvidenceV <grisbyar@mail.uc.edu>
2023-04-22 18:39:15 -0400

    more adjustments9

--- app/src/main/java/com/example/roomremix2/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/roomremix2/ui/about/AboutFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.roomremix2.databinding.FragmentAboutBinding
-
+//
 class AboutFragment : Fragment() {
 
     private var _binding: FragmentAboutBinding? = null
